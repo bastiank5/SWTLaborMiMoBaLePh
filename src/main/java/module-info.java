bc8fd377b7bdd2aa4005a2f7aeac2ae28485bb25
bastiank5 <bastian0.5@web.de>
2024-10-21 14:17:51 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.swtlabormimobaleph {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.swtlabormimobaleph to javafx.fxml;
+    exports org.example.swtlabormimobaleph;
+}
