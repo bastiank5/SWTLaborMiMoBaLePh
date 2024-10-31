83fce2a96f6de51abeebc7772158c9046fecaa00
BaumeisterMoritz <mobait02@hs-esslingen.de>
2024-10-31 14:46:02 +0100

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,4 +1,19 @@
 public class Employee {
+    private String firstname;
+    private String lastname;
+    private int id;
+    private String password;
+    private Float workLoad;
+    private Supervisor supervisor;
+    private Arraylist <DailyEntry>;
+
+    protected deleteVacation(){}
+    protected applyVacation(){}
+    protected changeVacation(){}
+    protected getEmployeeCalendar(){}
+    protected setEmployeeCalendar(){}
+
+    
     Employee() {
     }
 }
