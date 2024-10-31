df3a3fe0848b420b1223c13afb2796feeb346bf5
BaumeisterMoritz <mobait02@hs-esslingen.de>
2024-10-31 14:59:50 +0100

    Update DailyEntry.java

--- DailyEntry.java
+++ DailyEntry.java
@@ -1,2 +1,15 @@
-public class DailyEntry {
+public class DailyEntry implements serializable{
+  private Date date;
+  private Float begin;
+  private Float end;
+  private Float break;
+  private String absence;
+  private String comment;
+
+public Float getBreak(){}
+public Float getHoursAsIs(){}
+public Float getHoursTarget(){}
+  
+  DailyEntry(){}
+  
 }
