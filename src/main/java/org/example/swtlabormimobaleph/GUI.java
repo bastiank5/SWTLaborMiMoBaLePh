18bd57457e4f8ad4332d7bf37e04bb716b9331c2
Bastian <63063222+bastiank5@users.noreply.github.com>
2024-11-01 12:18:36 +0100

    Scene-Builder Installation

--- src/main/java/org/example/swtlabormimobaleph/GUI.java
+++ src/main/java/org/example/swtlabormimobaleph/GUI.java
@@ -1,7 +1,24 @@
 package org.example.swtlabormimobaleph;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import static javafx.application.Application.launch;
+
 public class GUI {
-    GUI(){
-        System.out.println("Test");
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public  void start(Stage primaryStage) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
