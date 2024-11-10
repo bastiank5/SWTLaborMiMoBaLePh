0955d1ef703dd540a2709400acd966c7bfd50f1b
Bastian <63063222+bastiank5@users.noreply.github.com>
2024-11-10 16:00:47 +0100

    Fenster schließen nicht mehr beim Aufruf von Messages

--- src/main/java/org/example/swtlabormimobaleph/GUI.java
+++ src/main/java/org/example/swtlabormimobaleph/GUI.java
@@ -29,7 +29,8 @@ import static org.example.swtlabormimobaleph.Absence.values;
 public class GUI extends Application {
     private boolean tableEditable = true;
 
-    private Stage ourPrimaryStage = new Stage();
+    private Stage ourPrimaryStage;
+    private Stage ourLoginStage;
     private static ArrayList<Message> test1 = new ArrayList<>();
     @FXML
     private TextField userField;
@@ -95,17 +96,19 @@ public class GUI extends Application {
     @FXML
     private TextField showEmployeeCalenderIDField;
 
+    private GUI mainController;
+    private Parent loginRoot;
+
     public GUI() throws IOException {
     }
 
-
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        ourPrimaryStage = primaryStage;
-        Parent loginRoot = FXMLLoader.load(getClass().getResource("LoginGUI.fxml"));
-        ourPrimaryStage.setTitle("Login");
-        ourPrimaryStage.setScene(new Scene(loginRoot));
-        ourPrimaryStage.show();
+    public void start(Stage Stage) throws IOException {
+        ourLoginStage = Stage;
+        loginRoot = FXMLLoader.load(getClass().getResource("LoginGUI.fxml"));
+        ourLoginStage.setTitle("Login");
+        ourLoginStage.setScene(new Scene(loginRoot));
+        ourLoginStage.show();
     }
     @FXML
     public void handleMessageAcceptButton(ActionEvent e){
@@ -118,7 +121,6 @@ public class GUI extends Application {
     @FXML
     public void handleMessageDenyButton(ActionEvent e){
         if(checkIfPossible()){
-
         }
     }
     private boolean checkIfPossible(){
@@ -137,11 +139,11 @@ public class GUI extends Application {
         }
     }
 
-
     @FXML
     private void handleLogin(ActionEvent event) {
         if (theSystem.getUserInformation(userField.getText(), passwordField.getText())) {
-            openMainWindow(event);
+            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            openMainWindow(loginStage);
         } else {
             System.out.println("Ungültige Anmeldeinformationen");
         }
@@ -154,7 +156,7 @@ public class GUI extends Application {
     }
 
     @FXML
-    private void handleshowEmployeeCalender(ActionEvent event) throws IOException {
+    private void handleshowEmployeeCalender() throws IOException {
         if (theSystem.currentUser instanceof Supervisor || theSystem.currentUser instanceof HR) {
             for (int i = 0; i < theSystem.employeeList.size(); i++) {
                 if (theSystem.employeeList.get(i).getId() == Integer.parseInt(showEmployeeCalenderIDField.getText())) {
@@ -162,7 +164,7 @@ public class GUI extends Application {
                 }
             }
             this.tableEditable = false;
-            openMainWindow(event);
+            openMainWindow(null);
         } else {
             System.out.println("Sorry, not permitted to show employee calender");
         }
@@ -171,7 +173,6 @@ public class GUI extends Application {
     @FXML
     private void handleShowMessages(ActionEvent event) {
         try {
-
             ArrayList<String> chats = theSystem.readCommunication();
             HashSet<String> set = new HashSet<>(chats);
             chats = new ArrayList<>(set);
@@ -196,37 +197,35 @@ public class GUI extends Application {
             System.out.println(test1.size());
             controller.messageTableView.setItems(chats2);
 
-
             Stage stage = new Stage();
             stage.setTitle("Messages");
             stage.setScene(new Scene(homeRoot));
             stage.show();
 
-            // Close the login window
-            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            loginStage.close();
+//            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//            loginStage.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @FXML
-    private void openMainWindow(ActionEvent event) {
+    private void openMainWindow(Stage stage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"));
             Parent homeRoot = loader.load();
-            GUI controller = loader.getController();
+            mainController = loader.getController();
 
-            controller.dayColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-            controller.weekdayColumn.setCellValueFactory(new PropertyValueFactory<>("weekday"));
-            controller.beginColumn.setCellValueFactory(new PropertyValueFactory<>("begin"));
-            controller.endColumn.setCellValueFactory(new PropertyValueFactory<>("end"));
-            controller.pauseColumn.setCellValueFactory(new PropertyValueFactory<>("pause"));
-            controller.hoursTargetColumn.setCellValueFactory(new PropertyValueFactory<>("hoursTarget"));
-            controller.hoursAsIsColumn.setCellValueFactory(new PropertyValueFactory<>("hoursAsIs"));
-            controller.diffColumn.setCellValueFactory(new PropertyValueFactory<>("diff"));
-            controller.absenceColumn.setCellValueFactory(new PropertyValueFactory<>("absence"));
-            controller.CommentColumn.setCellValueFactory(new PropertyValueFactory<>("comment"));
+            mainController.dayColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+            mainController.weekdayColumn.setCellValueFactory(new PropertyValueFactory<>("weekday"));
+            mainController.beginColumn.setCellValueFactory(new PropertyValueFactory<>("begin"));
+            mainController.endColumn.setCellValueFactory(new PropertyValueFactory<>("end"));
+            mainController.pauseColumn.setCellValueFactory(new PropertyValueFactory<>("pause"));
+            mainController.hoursTargetColumn.setCellValueFactory(new PropertyValueFactory<>("hoursTarget"));
+            mainController.hoursAsIsColumn.setCellValueFactory(new PropertyValueFactory<>("hoursAsIs"));
+            mainController.diffColumn.setCellValueFactory(new PropertyValueFactory<>("diff"));
+            mainController.absenceColumn.setCellValueFactory(new PropertyValueFactory<>("absence"));
+            mainController.CommentColumn.setCellValueFactory(new PropertyValueFactory<>("comment"));
 
             StringConverter<LocalTime> timeConverter = new StringConverter<LocalTime>() {
                 private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
@@ -245,114 +244,112 @@ public class GUI extends Application {
                     }
                 }
             };
-            controller.beginColumn.setCellFactory(TextFieldTableCell.forTableColumn(timeConverter));
-            controller.beginColumn.setOnEditCommit(ev -> {
+            mainController.beginColumn.setCellFactory(TextFieldTableCell.forTableColumn(timeConverter));
+            mainController.beginColumn.setOnEditCommit(ev -> {
                 DailyEntry entry = ev.getRowValue();
                 LocalTime newTime = ev.getNewValue();
 
                 entry.setBegin(String.valueOf(newTime));
 
                 theSystem.currentUser.updateCalender(entry);
-                controller.tableView.refresh();
-                controller.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
-                controller.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
+                mainController.tableView.refresh();
+                mainController.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
+                mainController.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
             });
 
-            controller.endColumn.setCellFactory(TextFieldTableCell.forTableColumn(timeConverter));
-            controller.endColumn.setOnEditCommit(ev -> {
+            mainController.endColumn.setCellFactory(TextFieldTableCell.forTableColumn(timeConverter));
+            mainController.endColumn.setOnEditCommit(ev -> {
                 DailyEntry entry = ev.getRowValue();
                 LocalTime newTime = ev.getNewValue();
 
                 entry.setEnd(String.valueOf(newTime));
 
                 theSystem.currentUser.updateCalender(entry);
-                controller.tableView.refresh();
-                controller.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
-                controller.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
+                mainController.tableView.refresh();
+                mainController.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
+                mainController.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
             });
 
-            controller.pauseColumn.setCellFactory(TextFieldTableCell.forTableColumn(timeConverter));
-            controller.pauseColumn.setOnEditCommit(ev -> {
+            mainController.pauseColumn.setCellFactory(TextFieldTableCell.forTableColumn(timeConverter));
+            mainController.pauseColumn.setOnEditCommit(ev -> {
                 DailyEntry entry = ev.getRowValue();
                 LocalTime newTime = ev.getNewValue();
 
                 entry.setPause(String.valueOf(newTime));
 
                 theSystem.currentUser.updateCalender(entry);
-                controller.tableView.refresh();
-                controller.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
-                controller.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
+                mainController.tableView.refresh();
+                mainController.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
+                mainController.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
             });
-            controller.absenceColumn.setCellValueFactory(new PropertyValueFactory<>("absence"));
+            mainController.absenceColumn.setCellValueFactory(new PropertyValueFactory<>("absence"));
             if (theSystem.currentUser instanceof HR || theSystem.currentUser instanceof Supervisor){
 
-                controller.absenceColumn.setCellFactory(ChoiceBoxTableCell.forTableColumn(
+                mainController.absenceColumn.setCellFactory(ChoiceBoxTableCell.forTableColumn(
                         FXCollections.observableArrayList(values())));
 
-                controller.absenceColumn.setOnEditCommit(ev -> {
+                mainController.absenceColumn.setOnEditCommit(ev -> {
                     DailyEntry entry = ev.getRowValue();
                     entry.setAbsence (ev.getNewValue());
 
                     theSystem.currentUser.updateCalender(entry);
-                    controller.tableView.refresh();
+                    mainController.tableView.refresh();
                 });}
 
 
-            controller.CommentColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-            controller.CommentColumn.setOnEditCommit(ev -> {
+            mainController.CommentColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+            mainController.CommentColumn.setOnEditCommit(ev -> {
                 DailyEntry entry = ev.getRowValue();
                 entry.setComment(ev.getNewValue());
 
                 theSystem.currentUser.updateCalender(entry);
-                controller.tableView.refresh();
+                mainController.tableView.refresh();
             });
 
             //Erstellung eines Test DailyEntry
             ObservableList<DailyEntry> dailyEntries = FXCollections.observableArrayList(
                     theSystem.currentUser.getCalendar()
             );
-            controller.tableView.setItems(dailyEntries);
+            mainController.tableView.setItems(dailyEntries);
 
             // Set label values through the controller
-            controller.firstnameLabel.setText(theSystem.currentUser.getFirstname());
-            controller.lastnameLabel.setText(theSystem.currentUser.getLastname());
-            controller.idLabel.setText(String.valueOf(theSystem.currentUser.getId()));
-            controller.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
-            controller.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
-
-
+            mainController.firstnameLabel.setText(theSystem.currentUser.getFirstname());
+            mainController.lastnameLabel.setText(theSystem.currentUser.getLastname());
+            mainController.idLabel.setText(String.valueOf(theSystem.currentUser.getId()));
+            mainController.workTimeLabel.setText(theSystem.workTime(theSystem.currentUser.getCalendar()));
+            mainController.flexTimeLabel.setText(theSystem.flexTime((theSystem.currentUser.getCalendar())));
 
             if(tableEditable == false){
-                controller.tableView.setEditable(false);
-                controller.CommentColumn.setEditable(false);
-                controller.absenceColumn.setEditable(false);
-                controller.beginColumn.setEditable(false);
-                controller.pauseColumn.setEditable(false);
-                controller.dayColumn.setEditable(false);
-                controller.diffColumn.setEditable(false);
-                controller.endColumn.setEditable(false);
+                mainController.tableView.setEditable(false);
+                mainController.CommentColumn.setEditable(false);
+                mainController.absenceColumn.setEditable(false);
+                mainController.beginColumn.setEditable(false);
+                mainController.pauseColumn.setEditable(false);
+                mainController.dayColumn.setEditable(false);
+                mainController.diffColumn.setEditable(false);
+                mainController.endColumn.setEditable(false);
             } else {
-                controller.tableView.setEditable(true);
-                controller.CommentColumn.setEditable(true);
-                controller.absenceColumn.setEditable(true);
-                controller.beginColumn.setEditable(true);
-                controller.pauseColumn.setEditable(true);
-                controller.dayColumn.setEditable(true);
-                controller.diffColumn.setEditable(true);
-                controller.endColumn.setEditable(true);
+                mainController.tableView.setEditable(true);
+                mainController.CommentColumn.setEditable(true);
+                mainController.absenceColumn.setEditable(true);
+                mainController.beginColumn.setEditable(true);
+                mainController.pauseColumn.setEditable(true);
+                mainController.dayColumn.setEditable(true);
+                mainController.diffColumn.setEditable(true);
+                mainController.endColumn.setEditable(true);
             }
-            //Labels hinzufügen und Berechnen
 
-            Stage stage = new Stage();
-            stage.setTitle("Hauptfenster");
-            stage.setScene(new Scene(homeRoot));
-            stage.show();
+            ourPrimaryStage = new Stage();
+            ourPrimaryStage.setTitle("Hauptfenster");
+            ourPrimaryStage.setScene(new Scene(homeRoot));
+            ourPrimaryStage.show();
 
             // Close the login window
-            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            loginStage.close();
+            if(stage != null) {
+                stage.close();
+            }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw  new RuntimeException(e);
         }
     }
 
